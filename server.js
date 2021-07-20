88c168e2de6bf9f474249bda70384bbaad1e5f13
Jay Thomas <thelastjet17@gmail.com>
2021-07-20 12:00:07 -0700

    2nd commit

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 let image = `<img src="https://images.westend61.de/0001251432pw/happy-male-and-female-computer-programmers-doing-high-five-over-computer-at-desk-in-creative-office-MASF13780.jpg" width="1000" height="600" />`;
 let html = `<!DOCTYPE html><html lang="en">
 <h1>Hello World!</h1>
-<h2>I will be getting a job soon</h2>
-<h2>Hopefully by the end of August</h2>
+<h2>I will be getting a job soon!</h2>
+<h2>Hopefully by the end of August, if not it WILL be by the end of September!</h2>
 ${image}
 </html>`;
 
